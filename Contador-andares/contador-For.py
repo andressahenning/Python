8c37fdfacbabcc83208ca6e4fd027fdf18614f68
andressahenning <henning.andressa1@gmail.com>
2023-12-18 16:34:48 -0300

    Initial commit

---
+++ Contador-andares/contador-For.py
@@ -0,0 +1,16 @@
+#ORDEM CRESCENTE
+
+for c in range(1, 21):
+    if c == 13:
+        continue
+    print("Andar:", c)
+print("FIM")
+
+
+#ORDEM DECRESCENTE 
+
+for c in range(20, 0, -1):
+    if c == 13:
+        continue
+    print("Andar:", c)
+print("FIM")
---
+++ Contador-andares/contador-While.py
@@ -0,0 +1,22 @@
+#ORDEM CRESCENTE
+
+c = 0
+
+while c < 20:
+    c += 1
+    if(c == 13):
+        continue
+    print("Andar:", c)
+print("FIM")
+
+
+#ORDEM DECRESCENTE
+
+c = 21
+
+while c > 1:
+    c -= 1
+    if(c == 13):
+        continue
+    print("Andar:", c)
+print("FIM")
